660f6cd37f31bc74cdc88ac736947694bd132b8e
hamed21q <hasanihamed220@gmail.com>
2024-08-05 23:58:58 -0700

    bug fix

--- internals/data_node.py
+++ internals/data_node.py
@@ -1,68 +1,28 @@
-from datetime import datetime
-from http import HTTPStatus
 from io import BytesIO
-from typing import Annotated, Optional
+from typing import Annotated
 
-from fastapi import FastAPI, File, UploadFile, Depends, HTTPException
-from pydantic import BaseModel, computed_field
-from redis import Redis
-from snowflake import SnowflakeGenerator
+from fastapi import FastAPI, File, UploadFile
 from starlette.responses import StreamingResponse
 
-from internals.conn import get_redis
-
 app = FastAPI()
-snow_flake_generator = SnowflakeGenerator(42)
-
-
-class ObjectModel(BaseModel):
-    file: Optional[bytes] = None
-    deleted: Optional[bool] = False
-
-    @computed_field
-    @property
-    def id(self) -> int:
-        return next(snow_flake_generator)
 
 
 @app.post("/files")
-async def create_file(
-        obj: Annotated[UploadFile, File(...)], redis: Annotated[Redis, Depends(get_redis)]
-):
-    with open("test.txt", "a") as file:
+async def create_file(obj: Annotated[UploadFile, File(...)]):
+    with open("test.txt", "ab") as file:
         # TODO: implement lock mechanism for file.tell() race condition possibility
-        model = ObjectModel(file=(await obj.read()))
-        size = file.write(model.model_dump_json())
-        meta = {
-            "offset": file.tell(),
-            "size": size,
-            "id": model.id,
-            "timestamp": datetime.utcnow().timestamp(),
-            "file_name": obj.filename,
-        }
-    redis.hset(meta["id"], mapping=meta)
-    return meta
-
-
-def get_params(file_id: str, redis: Annotated[Redis, Depends(get_redis)]):
-    params = redis.hgetall(file_id)
-    if not params:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
-    d = {}
-    for key, value in params.items():
-        try:
-            d[key.decode()] = int(value.decode())
-        except ValueError:
-            d[key.decode()] = value.decode()
-    return d
+        size = file.write((await obj.read()))
+        return {"id": f"{file.tell()}:{size}:{obj.filename}"}
 
 
 @app.get("/files/{file_id}")
-async def read_file(params: Annotated[dict, Depends(get_params)]):
-    with open("test.txt", "rb") as f:
-        f.seek(params['offset'] - params['size'])
+async def read_file(file_id: str):
+    params = file_id.split(":")
+    offset, size = map(int, params[:2])
+    with open("test.txt", 'rb') as f:
+        f.seek(offset - size)
         return StreamingResponse(
-            BytesIO(f.read(params['size'])),
+            BytesIO(f.read(size)),
             media_type="application/octet-stream",
-            headers={"Content-Disposition": f"attachment;filename={params['file_name']}"},
+            headers={"Content-Disposition": f"attachment;filename={params[2]}"},
         )
--- test/test_data_node.py
+++ test/test_data_node.py
@@ -1,4 +1,3 @@
-import json
 from http import HTTPStatus
 
 from httpx import AsyncClient
@@ -14,26 +13,8 @@ async def test_create_file(client: AsyncClient, file):
     )
 
     assert response.status_code == HTTPStatus.OK, response.json()
-    offset, size = response.json()['offset'], response.json()['size']
+    params = response.json()['id'].split(":")
+    offset, size = map(int, params[:2])
     with open("test.txt", 'rb') as f:
         f.seek(offset - size)
-        x = f.read(size).decode()
-        assert json.loads(x)['file'] == obj.decode()
-
-
-async def test_get_file(client: AsyncClient, file):
-    response = await client.post(
-        app.url_path_for("create_file"),
-        files={"obj": (file.name, file, "application/octet-stream")},
-    )
-    file_id = response.json()['id']
-
-    response = await client.get(app.url_path_for("read_file", file_id=file_id))
-
-    assert response.status_code == HTTPStatus.OK
-
-
-async def test_get_file_when_id_not_exists_then_raise_error(client, file):
-    response = await client.get(app.url_path_for("read_file", file_id="invalid_id"))
-
-    assert response.status_code == HTTPStatus.NOT_FOUND, response.json()
+        assert f.read(size) == obj
